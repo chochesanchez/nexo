08562123375fed4fec1972c611554c9b5b19638d
Choche Sanchez <chochesanchez2003@gmail.com>
2026-05-04 16:06:31 -0600

    Initial commit

---
+++ nexo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nexo
+//
+//  Created by José Manuel Sánchez Pérez on 04/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nexo/nexoApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexoApp.swift
+//  nexo
+//
+//  Created by José Manuel Sánchez Pérez on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct nexoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
